c9c5ce658c905004b7b6a366cb4d601e5def7683
Ninok199 <inno4ka1996@gmail.com>
2018-05-15 13:38:30 +0300

    add design1

--- app/src/main/java/com/example/diplom11/View/ConfigurationActivity.java
+++ app/src/main/java/com/example/diplom11/View/ConfigurationActivity.java
@@ -2,12 +2,14 @@ package com.example.diplom11.View;
 
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.diplom11.MainActivity;
 import com.example.diplom11.Presenters.BasePresenter;
@@ -20,12 +22,16 @@ public class ConfigurationActivity extends AppCompatActivity  {
     ListView menu;
     BasePresenter presenter;
     Intent intent;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
         menu = findViewById(R.id.menuItemsConfig);
+        textView =findViewById(R.id.textView);
+        textView.setTypeface(Typeface.createFromAsset(
+                getAssets(), "fonts/11364.ttf"));
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.itemsOfMenu, android.R.layout.simple_list_item_1);
         menu.setAdapter(adapter);
         presenter = new ConfigPresenterImpl(this);
